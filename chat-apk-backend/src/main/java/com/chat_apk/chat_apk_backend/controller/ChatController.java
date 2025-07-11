25f9666abbff23c2d58f817f65392606acc217ae
enlighttarunkumar <singhbaseraman@gmail.com>
2025-07-11 19:37:10 +0530

    chal ja

--- chat-apk-backend/src/main/java/com/chat_apk/chat_apk_backend/controller/ChatController.java
+++ chat-apk-backend/src/main/java/com/chat_apk/chat_apk_backend/controller/ChatController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.time.LocalDateTime;
 
 @Controller
-@CrossOrigin(AppConstant.URL)
+@CrossOrigin(origins = AppConstant.URL)
 public class ChatController {
 
     @Autowired
