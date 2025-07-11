f6f80ecb884d8ed980efebd6cf3dd3118858f75e
enlighttarunkumar <singhbaseraman@gmail.com>
2025-07-11 18:46:01 +0530

    setting connection

--- chat-frontend/src/config/AxiosHelper.js
+++ chat-frontend/src/config/AxiosHelper.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-export const baseURL = "http://localhost:8080";
-export const  httpClient  = axios.create(
-    {
-        baseURL: baseURL,
-    }
-);
+
+export const baseURL = import.meta.env.VITE_BACKEND_URL || "http://localhost:8080";
+
+export const httpClient = axios.create({
+  baseURL: baseURL,
+});
